3d6646fbfe6c8db5e1938b03df1c0b6b4f66bdc0
balamurugan213 <balainfo21@gmail.com>
2020-07-02 18:18:46 -0700

    implemented weather api

--- app/app.js
+++ app/app.js
@@ -1,96 +1,127 @@
-var mgodapp = angular.module('mgodapp', [
-	'ngRoute',
-	'ngAnimate'
-])
+var weatherApp = angular.module('weatherApp', [])
+const picture = [
+	'Clear',
+	'Rain',
+	'Haze',
+	'Mist',
+	'Snow',
+	'Drizzle',
+	'Thunderstorm',
+	'Clouds'
+]
 
-mgodapp.config([
-	'$routeProvider',
-	// '$locationProvider',
-	function($routeProvider /*, $locationProvider*/){
-		// $locationProvider.html5Mode(true)
-		// route to home
-		$routeProvider
-			.when('/home', {
-				templateUrl : './views/home.html',
-				controller  : 'godController'
-			})
-			// route to list
-			.when('/contact', {
-				templateUrl : './views/contact.html',
-				controller  : 'contactController'
-			})
-			.when('/list', {
-				templateUrl : './views/list.html',
-				controller  : 'godController'
-			})
-			.when('/contact-success', {
-				templateUrl : './views/contact-success.html'
-				//controller  : 'godController'
-			})
-			.otherwise({
-				redirectTo : '/home'
-			})
-	}
-])
+// mgodapp.config([
+// 	'$routeProvider',
+// 	// '$locationProvider',
+// 	function($routeProvider /*, $locationProvider*/){
+// 		// $locationProvider.html5Mode(true)
+// 		// route to home
+// 		$routeProvider
+// 			.when('/home', {
+// 				templateUrl : './views/home.html',
+// 				controller  : 'godController'
+// 			})
+// 			// route to list
+// 			.when('/contact', {
+// 				templateUrl : './views/contact.html',
+// 				controller  : 'contactController'
+// 			})
+// 			.when('/list', {
+// 				templateUrl : './views/list.html',
+// 				controller  : 'godController'
+// 			})
+// 			.when('/contact-success', {
+// 				templateUrl : './views/contact-success.html'
+// 				//controller  : 'godController'
+// 			})
+// 			.otherwise({
+// 				redirectTo : '/home'
+// 			})
+// 	}
+// ])
 
-mgodapp.directive('randomVal', [
-	function(){
-		return {
-			restrict    : 'E',
-			scope       : {
-				gods  : '=',
-				title : '='
-			},
-			templateUrl : './views/card.html', //'<h3>{{gods[random].name}}<h3>',
-			transclude  : true,
-			replace     : true,
-			controller  : function($scope){
-				$scope.random = Math.floor(
-					Math.random() * 4
-				)
-			}
-		}
-	}
-])
-
-mgodapp.run(function(){})
-mgodapp.controller('godController', [
+weatherApp.run(function(){})
+weatherApp.controller('WeatherController', [
 	'$scope',
 	'$http',
 	function($scope, $http){
-		// $scope.message = 'be the god'
-		$scope.removeg = function(g){
-			var rg = $scope.god.indexOf(g)
-			$scope.god.splice(rg, 1)
-		}
-		$scope.addToList = function(){
-			$scope.god.push({
-				name      : $scope.newg.name,
-				date      : $scope.newg.date,
-				detail    : $scope.newg.detail,
-				available : true
-			})
-			$scope.newg.name = ''
-			$scope.newg.date = ''
-			$scope.newg.detail = ''
+		$scope.ban = 'ban'
+		$scope.city = 'chennai'
+		$scope.day = 'Sunday'
+		$scope.maintemp = '0'
+		$scope.maxtemp = '0'
+		$scope.mintemp = '0'
+		$scope.main = 'Clouds'
+		$scope.pressure = '0'
+		$scope.humidity = '0'
+		$scope.clouds = '0'
+		$scope.feellike = '0'
+		$scope.desc = 'partially cloudy'
+		$scope.wind = '0'
+		$scope.lat = '0'
+		$scope.long = '0'
+		$scope.date = 'May 05'
+		$scope.time = '11.01'
+
+		$scope.Changeloc = function(){
+			$scope.city = $scope.cname
+			console.log($scope.city)
+			$scope.change()
+			$scope.cname = ''
 		}
 
-		$scope.removeAll = function(){
-			$scope.god = []
+		$scope.change = function(){
+			$http
+				.get(
+					'https://api.openweathermap.org/data/2.5/weather?q=' +
+						$scope.city +
+						'&units=metric&appid=8e9951a474e2bb4235b1768ea4355398'
+				)
+				.then(function(dat){
+					$scope.weather = dat.data
+					console.log($scope.weather)
+					$scope.day = 'Sunday'
+					$scope.maintemp =
+						$scope.weather.main.temp
+					$scope.maxtemp =
+						$scope.weather.main.temp_max
+					$scope.mintemp =
+						$scope.weather.main.temp_min
+					$scope.main =
+						$scope.weather.weather[0].main
+					$scope.pressure =
+						$scope.weather.main.pressure
+					$scope.humidity =
+						$scope.weather.main.humidity
+					$scope.clouds =
+						$scope.weather.clouds.all
+					$scope.feellike =
+						$scope.weather.main.feels_like
+					$scope.desc =
+						$scope.weather.weather[0].description
+					$scope.wind = $scope.weather.wind.speed
+					$scope.lat = $scope.weather.coord.lat
+					$scope.long = $scope.weather.coord.lon
+					$scope.date = 'May 05'
+					$scope.time = '11.01'
+					const back = document.querySelector(
+						'.background'
+					)
+					const icon = document.querySelector(
+						'.icon'
+					)
+					icon.src =
+						'./img/icons/' +
+						$scope.main +
+						'-01.svg'
+					back.classList.add($scope.main)
+					picture.forEach(function(p){
+						if (p != $scope.main) {
+							back.classList.remove(p)
+						}
+					})
+				})
 		}
-
-		$http.get('data/data.json').then(function(response){
-			$scope.god = response.data
-		})
-	}
-])
-
-mgodapp.controller('contactController', [
-	'$scope',
-	'$location',
-	function($scope, $location){
-		$scope.sendMessage = function(){
-			$location.path('/contact-success')
-		}
+		$scope.change()
 	}
 ])
